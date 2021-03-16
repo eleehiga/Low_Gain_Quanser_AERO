82fa3cf52d2ad698a28798c602f32a929a41f38f
Ethan Higa <55113159+eleehiga@users.noreply.github.com>
2021-03-16 14:28:40 -0700

    Turns out only supposed to change del as that allows for the input voltage to not saturate. However at this value for del the pitch does not go to the setpoint. The commented out Q values work however the system saturates.

--- ehiga_low_gain.m
+++ ehiga_low_gain.m
@@ -3,8 +3,10 @@ quanser_aero_parameters;
 % Load state-space matrices 
 quanser_aero_state_space;
 % Low Gain LQR controller
-del = 0.01;
+del = 300;
 Q = del * eye(4,4);
+%Q = diag([80000 100 0 0]) % works for simulation
+%Q = diag([80000 100000 0 0]) %somewhat works for actual machine
 [X, L, G] = care(A,B,Q)
 eig(A-B*G)
 
